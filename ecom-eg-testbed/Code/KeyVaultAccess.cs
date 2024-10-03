478c021c0cad0c63aaead2efbb163e8bb5fb747b
Michael Annesley <michael.annesley@commercevision.com.au>
2024-10-03 16:50:01 +1000

    DEVOPS-592
    update test bed app to read secrets

---
+++ ecom-eg-testbed/Code/KeyVaultAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Ajax.Utilities;
+
+namespace EcomEgTestBed.Code
+{
+    public static class KeyVaultAccess
+    {
+        public static string ReadSecret(string secretName, string keyVaultName)
+        {
+            if(string.IsNullOrEmpty(secretName)) return "ERROR: No secret name provided";
+
+            if(string.IsNullOrEmpty(keyVaultName)) return "ERROR: No key vault name provided";
+
+            // Replace with your Key Vault URL
+            string keyVaultUrl = $"https://{keyVaultName}.vault.azure.net/";
+                
+            // Create a SecretClient using DefaultAzureCredential which will use RBAC
+            var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+            try
+            {
+                // Get the secret
+                KeyVaultSecret secret = client.GetSecret(secretName);
+                return secret.Value;
+            }
+            catch (Exception ex)
+            {
+                return "Failed to read secret";
+            }
+        }
+    }
+}
